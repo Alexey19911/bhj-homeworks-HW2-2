24ff9903eff18f7059ab1558870cd4bb84c1ff8e
Sapega AS <olsapega@yandex.ru>
2023-06-15 20:24:45 +0700

    commit

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+let timerElement = document.getElementById('timer');
+let seconds = parseInt(timerElement.innerText);
+
+function countdown() {
+    seconds--; 
+    if (seconds <= 0) {
+         alert("Вы победили в конкурсе!");
+    } else {
+         timerElement.innerText = seconds;
+    }
+}
+setInterval(countdown, 1000);
